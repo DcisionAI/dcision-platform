56da8c6e9a60592c1c7793d910401166f241c7c6
Your Name <your.email@example.com>
2025-06-13 01:47:58 -0400

    deploy issues'

--- src/pages/api/auth/[...supabase].ts
+++
@@ -1,5 +0,0 @@
-import { handleAuth } from '@supabase/auth-helpers-nextjs';
-
-// Supabase Auth Helpers Next.js catch-all route
-// This will handle /api/auth/* endpoints for sign in, sign up, callback, etc.
-export default handleAuth();
